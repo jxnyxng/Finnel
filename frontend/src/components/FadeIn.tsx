47128acb0a4753bd51322298e00d3dfa9fd2e792
kjy <kim0607mi@smail.kongju.ac.kr>
2026-08-18 13:40:43 +0900

    [fix] DOM

--- frontend/src/components/FadeIn.tsx
+++ frontend/src/components/FadeIn.tsx
@@ -6,19 +6,23 @@ type FadeInProps = {
     delay?: number;
     duration?: number;
     className?: string;
-};
+} & React.HTMLAttributes<HTMLElement>;
 
 export function FadeIn({
                            children,
                            as: Component = 'div',
                            delay = 0,
                            duration = 0.4,
-                           className = ''
+                           className = '',
+                           style,
+                           ...props
                        }: FadeInProps) {
     return (
         <Component
+            {...props}
             className={className}
             style={{
+                ...style,
                 opacity: 0,
                 animation: `todayFlowFadeInUp ${duration}s ease-out ${delay}s forwards`
             }}
